7b65995d210040129afa06f99f04216109fa2275
dzvisa1 <dzvisaa@gmail.com>
2022-10-10 14:57:45 -0400

    test case created

---
+++ src/test/java/com/vytrack/tests/Isa_TS27_47.java
@@ -0,0 +1,48 @@
+package com.vytrack.tests;
+
+import com.vytrack.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Isa_TS27_47 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://qa2.vytrack.com/user/login");
+
+
+    }
+    @Test
+
+    public void VehicleContracts(){
+
+        WebElement userBox = driver.findElement(By.xpath("//input[@id='prependedInput']"));
+
+
+        WebElement passportBox=driver.findElement(By.xpath("//input[@type='password']"));
+
+
+        userBox.sendKeys("user150");
+
+        passportBox.sendKeys("UserUser123");
+
+        WebElement loginButton=driver.findElement(By.xpath("//div/button"));
+
+        loginButton.click();
+
+        WebElement fleetMagule =driver.findElement(By.xpath(""));
+
+
+
+
+    }
+}

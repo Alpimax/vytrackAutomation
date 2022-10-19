f9d2d74d8808a5d369d4b30e22db9d00660e5d3f
aqeelmukhtar <aqeelmukhtar04@gmail.com>
2022-10-19 10:10:50 -0400

    codes are updated

--- src/test/java/com/vytrack/tests/Aqeel_TS27_44.java
+++ src/test/java/com/vytrack/tests/Aqeel_TS27_44.java
@@ -5,10 +5,14 @@ import com.vytrack.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Aqeel_TS27_44 {
@@ -24,14 +28,67 @@ public class Aqeel_TS27_44 {
     }
 
 
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
+//    @AfterMethod
+//    public void tearDown() {
+//        driver.quit();
+//    }
 
     @Test
 
-    public void VehicleContractsForTruckDrivers() throws InterruptedException {
+    public void storeManagerModuleTest() throws InterruptedException {
+
+        WebElement userBox = driver.findElement(By.xpath("//input[@id='prependedInput']"));
+        userBox.sendKeys(ConfigurationReader.getProperty("StoreManUsr"));
+        Thread.sleep(2000);
+
+        WebElement passportBox = driver.findElement(By.xpath("//input[@type='password']"));
+        passportBox.sendKeys(ConfigurationReader.getProperty("password"));
+        Thread.sleep(2000);
+
+        WebElement clickLogin = driver.findElement(By.xpath("//button[@id='_submit']"));
+        clickLogin.click();
+        Thread.sleep(2000);
+
+        List<WebElement> modules = driver.findElements(By.xpath("//span[@class='title title-level-1']"));
+        List<String> expectedModules = new ArrayList<>(Arrays.asList("Dashboards", "Fleet", "Customers", "Sales", "Activities", "Marketing", "Reports & Segments", "System"));
+
+        for (int i = 0; i < modules.size(); i++) {
+            Assert.assertEquals(modules.get(i).getText(), expectedModules.get(i));
+            Assert.assertTrue(modules.get(i).isDisplayed());
+
+        }
+
+
+    }
+    @Test
+    public void salesManagerModuleTest() throws InterruptedException {
+
+        WebElement userBox = driver.findElement(By.xpath("//input[@id='prependedInput']"));
+        userBox.sendKeys(ConfigurationReader.getProperty("SalesManUsr"));
+        Thread.sleep(2000);
+
+        WebElement passportBox = driver.findElement(By.xpath("//input[@type='password']"));
+        passportBox.sendKeys(ConfigurationReader.getProperty("password"));
+        Thread.sleep(2000);
+
+        WebElement clickLogin = driver.findElement(By.xpath("//button[@id='_submit']"));
+        clickLogin.click();
+        Thread.sleep(2000);
+
+        List<WebElement> modules = driver.findElements(By.xpath("//span[@class='title title-level-1']"));
+        List<String> expectedModules = new ArrayList<>(Arrays.asList("Dashboards", "Fleet", "Customers", "Sales", "Activities", "Marketing", "Reports & Segments", "System"));
+
+        for (int i = 0; i < modules.size(); i++) {
+            Assert.assertEquals(modules.get(i).getText(), expectedModules.get(i));
+            Assert.assertTrue(modules.get(i).isDisplayed());
+
+        }
+
+
+    }
+
+    @Test
+    public void TruckDriverModuleTest() throws InterruptedException {
 
         WebElement userBox = driver.findElement(By.xpath("//input[@id='prependedInput']"));
         userBox.sendKeys(ConfigurationReader.getProperty("UserUser"));
@@ -45,8 +102,19 @@ public class Aqeel_TS27_44 {
         clickLogin.click();
         Thread.sleep(2000);
 
+        List<WebElement> modules = driver.findElements(By.xpath("//span[@class='title title-level-1']"));
+        List<String> expectedModules = new ArrayList<>(Arrays.asList("Fleet", "Customers","Activities", "System"));
+
+        for (int i = 0; i < modules.size(); i++) {
+            Assert.assertEquals(modules.get(i).getText(), expectedModules.get(i));
+            Assert.assertTrue(modules.get(i).isDisplayed());
+
+        }
+
 
     }
+
+
 
 
 }
